1cbb8152d93823bafd91c7fbb21076fbbc7c6ac8
ducanhnt22 <ducanhbon4@gmail.com>
2024-03-24 14:29:38 +0700

    done auth

--- src/app/(auth)/SignIn/SignIn.tsx
+++ src/app/(auth)/SignIn/SignIn.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { signIn } from 'next-auth/react';
 import Image from 'next/image';
 import React, { useState } from 'react'
 
@@ -11,27 +12,20 @@ export default function SignIn() {
         e.preventDefault();
         setIsLoggingIn(true);
         try {
-            const response = await fetch("/api/v1/User/login", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify({
-                    securityCode,
-                    password,
-                }),
+            const res = await signIn("credentials", {
+                securityCode,
+                password,
+                redirect: true,
+                callbackUrl: "/",
             });
-            if (response.ok) {
-                const data = await response.json();
-                console.log(data);
-            }
+            console.log(res);
         } catch (error) {
-            console.error("An unexpected error happened:", error);
+            console.log(error);
         }
-        setIsLoggingIn(false);
-    }
+    };
+
     return (
-        <div className='flex flex-col md:flex-row h-screen items-center'>
+        <div className='flex flex-col md:flex-row h-screen items-center justify-center'>
             <div className="bg-indigo-600 hidden lg:block w-full md:w-1/2 xl:w-2/3 h-screen">
                 <img src="https://source.unsplash.com/random" alt="" className="w-full h-full object-cover" />
             </div>
@@ -39,24 +33,41 @@ export default function SignIn() {
                 <div className="w-full h-100">
                     <h1 className="text-xl md:text-2xl font-bold leading-tight mt-12">Log in to your account</h1>
 
-                    <form className="mt-6" action="#" method="POST">
+                    <form className="mt-6" onSubmit={(e) => handleSignin(e)}>
                         <div>
-                            <label className="block text-gray-700">Security Code</label>
-                            <input type="text" name="" id="" placeholder="Enter Security Code" className="w-full px-4 py-3 rounded-lg bg-gray-200 mt-2 border focus:border-blue-500 focus:bg-white focus:outline-none" autoFocus required />
+                            <label htmlFor="securityCode" className="block text-gray-700">Security Code</label>
+                            <input 
+                                type="text" 
+                                id="securityCode" 
+                                placeholder="Enter Security Code" 
+                                className="w-full px-4 py-3 rounded-lg bg-gray-200 mt-2 border focus:border-blue-500 focus:bg-white focus:outline-none" 
+                                autoFocus 
+                                required 
+                                // value={securityCode} 
+                                onChange={(e) => setSecurityCode(e.target.value)}
+                            />
                         </div>
 
                         <div className="mt-4">
-                            <label className="block text-gray-700">Password</label>
-                            <input type="password" name="" id="" placeholder="Enter Password" className="w-full px-4 py-3 rounded-lg bg-gray-200 mt-2 border focus:border-blue-500
-                focus:bg-white focus:outline-none" required />
+                            <label htmlFor="password" className="block text-gray-700">Password</label>
+                            <input 
+                                type="password" 
+                                id="password" 
+                                placeholder="Enter Password" 
+                                className="w-full px-4 py-3 rounded-lg bg-gray-200 mt-2 border focus:border-blue-500 focus:bg-white focus:outline-none" 
+                                required 
+                                // value={password} 
+                                onChange={(e) => setPassword(e.target.value)}
+                            />
                         </div>
 
                         <div className="text-right mt-2">
                             <a href="#" className="text-sm font-semibold text-gray-700 hover:text-blue-700 focus:text-blue-700">Forgot Password?</a>
                         </div>
 
-                        <button type="submit" className="w-full block bg-indigo-500 hover:bg-indigo-400 focus:bg-indigo-400 text-white font-semibold rounded-lg
-              px-4 py-3 mt-6">Log In</button>
+                        <button className="w-full block bg-indigo-500 hover:bg-indigo-400 focus:bg-indigo-400 text-white font-semibold rounded-lg px-4 py-3 mt-6">
+                            Log In
+                        </button>
                     </form>
                 </div>
             </div>
--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -4,29 +4,34 @@ import CredentialsProvider from "next-auth/providers/credentials";
 export const authOptions: NextAuthOptions = {
   providers: [
     CredentialsProvider({
-      name: "firecameraalarmsolution",
+      name: "firealarmcamerasolution",
       credentials: {
-        securitycode: { label: "securitycode", type: "text" },
+        securityCode: { label: "securityCode", type: "text" },
         password: { label: "password", type: "password" },
       },
 
-      async authorize(credentials, req) {
-        const res = await fetch(`${process.env.API_URL}/user/login`, {
-          method: "POST",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify({
-            email: credentials?.securitycode,
-            password: credentials?.password,
-          }),
-          cache: "no-cache",
-        });
+      async authorize(credentials) : Promise<any> {
+        try {
+          const res = await fetch(`${process.env.API_URL}/User/login`, {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+              securityCode: credentials?.securityCode,
+              password: credentials?.password,
+            }),
+            cache: "no-cache",
+          });
+  
+          const user = await res.json();
+          console.log(user);
 
-        const user = await res.json();
-
-        if (res.ok && user) {
-          return user;
-        } else {
-          return null;
+          if (res.ok && user) {
+            return user;
+          } else {
+            return null;
+          }
+        } catch (err) {
+          console.log(err);
         }
       },
     }),
