91d168368733ed2d427976ec22a9c38c37cc6331
HLong <longlhse161497@fpt.edu.vn>
2024-04-02 23:32:17 +0700

    temp fe for users &  record list

--- src/app/dashboard/record/page.tsx
+++ src/app/dashboard/record/page.tsx
@@ -3,7 +3,7 @@ import { getRecords } from '@/app/lib';
 import { IRecords } from '@/app/types';
 import { getServerSession } from 'next-auth';
 import React from 'react';
-import RecordRow from './components/RecordTable';
+import RecordTable from './components/RecordTable';
 
 export default async function Record(token: string | undefined) {
   const session = await getServerSession(authOptions);
@@ -11,34 +11,9 @@ export default async function Record(token: string | undefined) {
   const listRecords: IRecords = await getRecords(token);
 
   return (
-    <div className="container mx-auto">
+    listRecords && <div className="container mx-auto">
       <h1 className="text-2xl font-bold my-4">Record List</h1>
-      <table className="table-auto w-full">
-        <thead>
-          <tr>
-            <th className="px-4 py-2">Status</th>
-            <th className="px-4 py-2">Record Time</th>
-            <th className="px-4 py-2">User Rating Percent</th>
-            <th className="px-4 py-2">Predicted Percent</th>
-            <th className="px-4 py-2">Created Date</th>
-            <th className="px-4 py-2">Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          {listRecords.results.map((record) => (
-            <RecordRow
-              key={record.id}
-              recordId={record.id}
-              status={record.status}
-              recordTime={record.recordTime}
-              userRatingPercent={record.userRatingPercent}
-              predictedPercent={record.predictedPercent}
-              createdDate={record.createdDate}
-              token={token}
-            />
-          ))}
-        </tbody>
-      </table>
+      <RecordTable body={listRecords} token={token} />
     </div>
   );
 }
